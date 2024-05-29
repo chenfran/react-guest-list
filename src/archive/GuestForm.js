9f2ec0649f2ea6945a61defd4ba2c6f182e6e95c
Franziska Chen <chenfranziska@gmail.com>
2024-05-30 00:26:41 +0200

    Remove useEffect in GuestForm.js and GuestList.js

--- src/archive/GuestForm.js
+++ src/archive/GuestForm.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export default function GuestForm({ addGuest }) {
   const [firstName, setFirstName] = useState('');
--- src/archive/GuestList.js
+++ src/archive/GuestList.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid'; // Import library to create unique IDs
 import Guest from './Guest';
 import GuestForm from './GuestForm';
